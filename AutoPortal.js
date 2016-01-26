91bddc849b38433a0b2ef77164ec81967e072140
Ishakaru <Ishakaru@users.noreply.github.com>
2016-01-26 08:06:04 -0500

    Trying another tact

--- AutoPortal.js
+++ AutoPortal.js
@@ -1,23 +1,17 @@
 // Place to handle all autoPortal logic
 
-init()
-function init()
-{
-  //create the elements, and place them in the right place.
-  var btn = document.createElement("DIV");
-  btn.setAttribute('id','autoP');
-  btn.setAttribute('class', 'noselect pointer challengeThing thing ');
-  btn.setAttribute("onclick", 'selectAutoPortalSettings()');
-  var spn =document.createElement("span");
-  spn.setAttribute('class','thingName');
-  spn.textContent='Auto Portal';
-  btn.appendChild(spn);
-  
-  document.getElementById("challengesHere").appendChild(btn);
+setTimeout(autoPortal, 2000);
 
-}
-function selectAutoPortalSettings()
+function autoPortal()
 {
-//test
-document.getElementById("specificChallengeDescription").innerHTML = '<div class="noselect pointer challengeThing thing " id="challengeElectricity" onclick="selectChallenge("Electricity")"><span class="thingName">Electricity</span></div>';
+    console.log("got here");
+    if(game.global.challengeActive === "")
+    {
+        portalClicked();
+        buyPortalUpgrade('Carpentry');
+        selectChallenge('Electricity');
+        activateClicked();
+        activatePortal();
+    }
+    
 }
