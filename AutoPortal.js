7b83daa522ce214627e1a986c85a5a5e89acd7c3
Ishakaru <Ishakaru@users.noreply.github.com>
2016-01-27 06:24:27 -0500

    Push Logic
    Added some push logic until I can get the interface squared away.

--- AutoPortal.js
+++ AutoPortal.js
@@ -1,12 +1,18 @@
 // Place to handle all autoPortal logic
 
 setTimeout(autoPortal, 2000);
-
+var chal_fin=false;
+var chal_fin_time=0
 function autoPortal()
 {
-    //console.log("got here");
-    if(game.global.challengeActive === "")
+    if(!chal_fin && game.global.challengeActive === "")
     {
+        chal_fin=true;
+        chal_fin_time=getMinutesThisPortal();
+    }
+    if(chal_fin && getMinutesThisPortal()-chal_fin_time>60)
+    {
+        chal_fin=false;
         portalClicked();
         buyPortalUpgrade('Carpentry');
         selectChallenge('Electricity');
